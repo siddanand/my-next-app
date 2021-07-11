43d25cc42f42be264b4678fc247b8f4f0dbc2c87
siddanand <chandiboy18@gmail.com>
2021-07-12 04:27:26 +0530

    Firebase Auth Setup Via Google

---
+++ services/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config={
+  apiKey: "AIzaSyC9WwlMleibGsQN7t-otS5F8jmDBxWwj8A",
+   authDomain: "nextjs-with-firebaseauth-9a783.firebaseapp.com",
+   projectId: "nextjs-with-firebaseauth-9a783",
+   storageBucket: "nextjs-with-firebaseauth-9a783.appspot.com",
+   messagingSenderId: "595636184612",
+   appId: "1:595636184612:web:26ce3c28da96add2a71ab1"
+}
+
+
+export default function initFirebase(){
+  if(!firebase.apps.length){
+    firebase.initializeApp(config);
+  }
+}
