1d553b2f57abe26239ea630cfd7a28e002992cad
siddanand <chandiboy18@gmail.com>
2021-07-11 18:50:03 +0530

    Home Page Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
+      />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
