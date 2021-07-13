d6012117a449a6546d5acf5fd2e62f15bc5a8cb4
siddanand <chandiboy18@gmail.com>
2021-07-14 04:43:02 +0530

    Prettier Setup For Pre-Commit

--- helpers/withAuth.js
+++ helpers/withAuth.js
@@ -1,44 +1,40 @@
-import React from 'react';
-import {Component} from 'react';
-import router from 'next/router';
-import 'firebase/auth';
-import firebase from 'firebase/app';
+import React from "react";
+import { Component } from "react";
+import router from "next/router";
+import "firebase/auth";
+import firebase from "firebase/app";
 
-import initFirebase from '../services/firebase.js';
+import initFirebase from "../services/firebase.js";
 initFirebase();
 
 const withAuth = (Component) => {
   class withAuth extends React.Component {
     state = {
-      status: 'Loading',
+      status: "Loading",
     };
-    componentDidMount=()=>{
-      firebase.auth().onAuthStateChanged(authUser => {
-console.log(authUser);
-if(authUser) {
-this.setState({
-status: 'SIGNED_IN'
-});
-}else {
-router.push('/');
-}
-});
-    }
+    componentDidMount = () => {
+      firebase.auth().onAuthStateChanged((authUser) => {
+        console.log(authUser);
+        if (authUser) {
+          this.setState({
+            status: "SIGNED_IN",
+          });
+        } else {
+          router.push("/");
+        }
+      });
+    };
     renderContent() {
-  const { status } = this.state;
-  if(status == 'LOADING') {
-  return <h1>Loading ......</h1>;
-  }else if(status == 'SIGNED_IN') {
-  return <Component { ...this.props } />
-  }
-  }
-  render() {
-    return (
-      <React.Fragment>
-      {this.renderContent()}
-      </React.Fragment>
-    );
-  }
+      const { status } = this.state;
+      if (status == "LOADING") {
+        return <h1>Loading ......</h1>;
+      } else if (status == "SIGNED_IN") {
+        return <Component {...this.props} />;
+      }
+    }
+    render() {
+      return <React.Fragment>{this.renderContent()}</React.Fragment>;
+    }
   }
   return withAuth;
 };
--- pages/login.js
+++ pages/login.js
@@ -1,68 +1,75 @@
-import firebase from 'firebase/app';
-import {useState} from 'react';
-import 'firebase/auth';
-import { useRouter } from 'next/router';
-import initFirebase from '../services/firebase.js';
+import firebase from "firebase/app";
+import { useState } from "react";
+import "firebase/auth";
+import { useRouter } from "next/router";
+import initFirebase from "../services/firebase.js";
 
 initFirebase();
 
-const provider =  new firebase.auth.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-export default function Join(){
- const router = useRouter()
+export default function Join() {
+  const router = useRouter();
   const [authorizing, setAuthorizing] = useState(false);
 
-
-  const handleAuthentication = async () =>{
-
+  const handleAuthentication = async () => {
     setAuthorizing(true);
     console.log(10);
 
-      const result = await firebase.auth().signInWithPopup(provider);
+    const result = await firebase.auth().signInWithPopup(provider);
 
-      const {user} = result;
-      console.log(result.additionalUserInfo.isNewUser);
-      console.log({user});
+    const { user } = result;
+    console.log(result.additionalUserInfo.isNewUser);
+    console.log({ user });
 
-      if(!user){
-        throw new Error('There was an issue authorizing');
-      }
-      setAuthorizing(false);
-      router.push('/dashboard');
+    if (!user) {
+      throw new Error("There was an issue authorizing");
     }
-    function currentUser() {
-      // [START auth_current_user]
-      const user = firebase.auth().currentUser;
+    setAuthorizing(false);
+    router.push("/dashboard");
+  };
+  function currentUser() {
+    // [START auth_current_user]
+    const user = firebase.auth().currentUser;
 
-      if (user) {
-        console.log(user)
-      } else {
-        console.log('no user')
-        // No user is signed in.
-      }
-      // [END auth_current_user]
+    if (user) {
+      console.log(user);
+    } else {
+      console.log("no user");
+      // No user is signed in.
     }
-    function signOut() {
-  // [START auth_sign_out]
-  firebase.auth().signOut().then(() => {
-    currentUser();
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-  // [END auth_sign_out]
-}
+    // [END auth_current_user]
+  }
+  function signOut() {
+    // [START auth_sign_out]
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        currentUser();
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+    // [END auth_sign_out]
+  }
 
-return (
-  <div>
-  <center>
-<h1>Join Now</h1>
-<h2>Click Below to Sign in or Join</h2>
-<button onClick = {handleAuthentication} className="btn btn-success" style={{outline:'none', marginTop:'30px'}}>Login Via Google</button>
-  <button onClick = {currentUser}>Current User</button>
-  <button onClick = {signOut}>Sign Out</button>
-
-</center>
-  </div>
-)
+  return (
+    <div>
+      <center>
+        <h1>Join Now</h1>
+        <h2>Click Below to Sign in or Join</h2>
+        <button
+          onClick={handleAuthentication}
+          className="btn btn-success"
+          style={{ outline: "none", marginTop: "30px" }}
+        >
+          Login Via Google
+        </button>
+        <button onClick={currentUser}>Current User</button>
+        <button onClick={signOut}>Sign Out</button>
+      </center>
+    </div>
+  );
 }
